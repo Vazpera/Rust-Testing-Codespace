97d5c3f61d56aed9e46c4c8d3ad5348e53e8f22a
Vaspera <110002885+Vazpera@users.noreply.github.com>
2024-06-01 21:43:04 +0000

    name change

---
+++ tournament/target/debug/deps/tournament-b868fab4bd20df82.d
@@ -0,0 +1,5 @@
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/libtournament-b868fab4bd20df82.rmeta: src/main.rs
+
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/tournament-b868fab4bd20df82.d: src/main.rs
+
+src/main.rs:
---
+++ tournament/target/debug/deps/tournament-bd63c0ea4fe3991d.d
@@ -0,0 +1,5 @@
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/libtournament-bd63c0ea4fe3991d.rmeta: src/main.rs
+
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/tournament-bd63c0ea4fe3991d.d: src/main.rs
+
+src/main.rs:
