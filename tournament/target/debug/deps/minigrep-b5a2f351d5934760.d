b6d099379950ae76552b50ac236e4458051be4c1
Vaspera <110002885+Vazpera@users.noreply.github.com>
2024-06-01 21:42:18 +0000

    idk at this point

--- tournament/target/debug/deps/minigrep-b5a2f351d5934760.d
+++ tournament/target/debug/deps/minigrep-b5a2f351d5934760.d
@@ -1,5 +1,5 @@
-/workspaces/Rust-Testing-Codespace/minigrep/target/debug/deps/libminigrep-b5a2f351d5934760.rmeta: src/main.rs
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/libminigrep-b5a2f351d5934760.rmeta: src/main.rs
 
-/workspaces/Rust-Testing-Codespace/minigrep/target/debug/deps/minigrep-b5a2f351d5934760.d: src/main.rs
+/workspaces/Rust-Testing-Codespace/tournament/target/debug/deps/minigrep-b5a2f351d5934760.d: src/main.rs
 
 src/main.rs:
