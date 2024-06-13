e02f25dbc3e2c1a5780850efac5eebc6f515b020
Vaspera <110002885+Vazpera@users.noreply.github.com>
2024-06-13 19:13:47 +0000

    Added functionality

--- tokio/src/vec3.rs
+++ tokio/src/vec3.rs
@@ -26,6 +26,39 @@ impl std::ops::Sub<Vec3> for Vec3 {
     }
 }
 
+impl std::ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        return Vec3 {
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        };
+    }
+}
+
+impl std::ops::Mul<Vec3> for f32 {
+    type Output = Vec3;
+    fn mul(self, rhs: Vec3) -> Self::Output {
+        return Vec3 {
+            x: self * rhs.x,
+            y: self * rhs.y,
+            z: self * rhs.z,
+        };
+    }
+}
+
+impl std::ops::Mul<f32> for Vec3 {
+    type Output = Vec3;
+    fn mul(self, rhs: f32) -> Self::Output {
+        return Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        };
+    }
+}
+
 pub fn new(x: f32, y: f32, z: f32) -> Vec3 {
     return Vec3 { x, y, z };
 }
